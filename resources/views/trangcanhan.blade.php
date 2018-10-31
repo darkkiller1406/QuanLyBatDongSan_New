8968568fa9a24240739e530fb1f80869b0a58bb9
Minh Nguyen <darkkiller1406@yahoo.com>
2018-10-31 13:48:08 +0700

    Remove paypal

--- resources/views/trangcanhan.blade.php
+++ resources/views/trangcanhan.blade.php
@@ -70,7 +70,6 @@
       </div>
       <div class="col-md-5"></div>
       <div class="col-md-2"><button type="button" onclick="capnhattien()" id="submitbtn" name="submitbtn" class="btn south-btn" >NẠP TIỀN</button></div>
-      <div id="paypal-button"></div>
       </div>
     </div>
   </div>
@@ -222,61 +221,5 @@
       }); 
     }         
   }
-</script>
-<script src="https://www.paypalobjects.com/api/checkout.js"></script>
-<script>
-  paypal.Button.render({ 
-    // Configure environment
-    env: 'sandbox',
-    client: {
-      sandbox: 'ATTVo7WpMwnwflDod16ao8yHN-2ZvMV1Iz3WuFkeBSJkKL6dkBC0IEJirVoaUG3qpPAIyprlSfl949Z1',
-      production: 'demo_production_client_id'
-    },
-    // Customize button (optional)
-    locale: 'en_US',
-    style: {
-      size: 'small',
-      color: 'gold',
-      shape: 'pill',
-    },
-    // Set up a payment
-  payment: function(data, actions) {
-  return actions.payment.create({
-    transactions: [{
-      amount: {
-        total: '0.1',
-        currency: 'USD',
-      },
-      description: 'Nạp tiền',
-      custom: 'YZ6TEDSHE4K4L',
-      //invoice_number: '12345', Insert a unique invoice number
-      payment_options: {
-        allowed_payment_method: 'INSTANT_FUNDING_SOURCE'
-      },
-      soft_descriptor: 'ECHI5786786',
-      item_list: {
-        shipping_address: {
-          recipient_name: 'Minh Nguyễn',
-          line1: '169 Tô Hiến Thành, Phường 13, Quận 10',
-          city: 'Hồ Chí Minh',
-          country_code: 'VN',
-          postal_code: '71000',
-          phone: '0569885811',
-          state: 'VN'
-        }
-      }
-    }],
-    note_to_payer: 'Contact us for any questions on your order.'
-  });
-},
-    // Execute the payment
-    onAuthorize: function(data, actions) {
-      return actions.payment.execute().then(function() {
-        // Show a confirmation message to the buyer
-        window.alert('Thank you for your purchase!');
-      });
-    }
-  }, '#paypal-button');
-
 </script>
 @endsection
